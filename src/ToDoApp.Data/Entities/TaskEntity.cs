36a7ec64abfccda3bece88f45c56cbb99ed8fe4f
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-08-22 13:11:11 +0300

    Add entities

---
+++ src/ToDoApp.Data/Entities/TaskEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDoApp.Data.Entities
+{
+    public class TaskEntity : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreationDatetime { get; set; }
+
+        public UserEntity User { get; set; } = null!;
+    }
+}
---
+++ src/ToDoApp.Data/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoApp.Data.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public IEnumerable<TaskEntity> Tasks { get; set; }
+    }
+}
