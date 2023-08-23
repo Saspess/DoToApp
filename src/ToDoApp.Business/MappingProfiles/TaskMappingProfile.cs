1888791e6da0bbc571acbf5f0e124c3a0c82e71e
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-08-23 12:54:25 +0300

    Add mapping profiles

---
+++ src/ToDoApp.Business/MappingProfiles/TaskMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ToDoApp.Business.DTOs.Task;
+using ToDoApp.Data.Entities;
+
+namespace ToDoApp.Business.MappingProfiles
+{
+    public class TaskMappingProfile : Profile
+    {
+        public TaskMappingProfile()
+        {
+            CreateMap<TaskEntity, TaskViewDto>()
+                .ForMember(dest => dest.AuthorsFirstName, opt => opt.MapFrom(src => src.AppUser.FirstName))
+                .ForMember(dest => dest.AuthorsLastName, opt => opt.MapFrom(src => src.AppUser.LastName))
+                .ForMember(dest => dest.AuthorsEmail, opt => opt.MapFrom(src => src.AppUser.Email));
+
+            CreateMap<TaskCreateDto, TaskEntity>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreationDatetime, opt => opt.MapFrom(src => DateTime.Now))
+                .ForMember(dest => dest.IsCompleted, opt => opt.MapFrom(src => false));
+
+            CreateMap<TaskUpdateDto, TaskEntity>();
+        }
+    }
+}
