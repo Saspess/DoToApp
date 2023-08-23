5aaf97131b46fa3700dd5c581cd85b7de774185e
Kolya Voitekhovich <kolya.vaitihovich09@gmail.com>
2023-08-23 11:37:20 +0300

    Add dtos

---
+++ src/ToDoApp.Business/DTOs/AppUser/AppUserUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApp.Business.DTOs.AppUser
+{
+    public class AppUserUpdateDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+    }
+}
